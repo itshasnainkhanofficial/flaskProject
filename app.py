36272a02c7d9d9f98953d9a6d9c30eaa86e3b7e9
hasnain <hasnainkhankqs@gmail.com>
2021-01-03 21:51:46 +0500

    registration working fine alhamdulillah

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , request , redirect , flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import bcrypt
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), nullable=False)
+    useremail = db.Column(db.String(200), nullable=False)
+    userpassword = db.Column(db.String(900), nullable=False)
+    date_created = db.Column(db.DateTime , default = datetime.utcnow)
+
+    def __repr__(self):
+        return  "<Task %r>" % self.id
+
+
 
 @app.route('/')
 def index():
@@ -10,6 +29,37 @@ def index():
 def contact():
     return render_template("contact.html")
 
+@app.route('/register')
+def register():
+    return render_template("registration.html")
+
+@app.route('/allusers')
+def allusers():
+    AllUsers = User.query.order_by(User.date_created).all()
+    return render_template("allUsers.html" ,  allusers = AllUsers)
+
+@app.route('/registerUser' , methods=["GET", "POST"])
+def registerUser():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['pass']
+
+        hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+        register = User(username = name, useremail = email, userpassword = hashed)
+
+        try:
+            db.session.add(register)
+            db.session.commit()
+            flash("You have been registered")
+            return redirect("/")
+        except Exception as e:
+            print(e)
+            return "Error occur during registration"
+
+    else:
+        return redirect("/allusers")
 
 
 @app.route('/about')
@@ -19,5 +69,6 @@ def about():
     return render_template("about.html" , about = frompython , list = list)
 
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
     #DEBUG is SET to TRUE. CHANGE FOR PROD
     app.run(debug=True)
